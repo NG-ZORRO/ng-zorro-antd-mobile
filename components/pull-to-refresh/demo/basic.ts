8725c4bc3a80f147af0395bfeb1a695299a8ed60
BronzeCui <15120056@bjtu.edu.cn>
2018-09-30 17:33:22 +0800

    fix(demo:pulltorefresh): fix demo pulltorefresh

--- components/pull-to-refresh/demo/basic.ts
+++ components/pull-to-refresh/demo/basic.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'demo-pull-to-refresh-basic',
   template: `
+    <NoticeBar *ngIf="!isMobile"
+               style="margin-bottom: 10px"
+               [noticeBarcontent]="'该组件只支持Touch事件，请使用移动模式/设备打开此页。'"
+               [marqueeProps]="{fps: 100}"
+    ></NoticeBar>
     <div Button style="marginBottom: 15px" (onClick)="onClick()">direction: {{this.state.down ? 'down' : 'up'}}</div>
     <PullToRefresh [ngStyle]="dtPullToRefreshStyle"
-                   [direction]="this.state.down ? 'down' : 'up'" [indicator]="this.state.down ? {} : { deactivate: '上拉可以刷新' }"
+                   [direction]="this.state.down ? 'down' : 'up'"
+                   [indicator]="this.state.down ? {} : { deactivate: '上拉可以刷新' }"
                    (onRefresh)="pullToRefresh($event)"
     >
       <div *ngFor="let i of this.state.data" style="text-align: center; padding: 20px">{{i}}</div>
@@ -16,6 +23,7 @@ import { Component, OnInit } from '@angular/core';
   `
 })
 export class DemoPullToRefreshBasicComponent implements OnInit {
+  isMobile = /Android|webOS|iPhone|iPod|BlackBerry/i.test(window.navigator.userAgent);
   pageLimit = 100;
   page = 0;
   state = {
