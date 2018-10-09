9b8f3da84da21e7fbe7c59419a68aebfa219b39f
Zhenyun.Wu <huihui.whh@alibaba-inc.com>
2018-10-09 22:37:00 +0800

    fix(module: input-item): fixed custom-keyboard hide (#21)

--- components/input-item/custom-keyboard/custom-keyboard.component.ts
+++ components/input-item/custom-keyboard/custom-keyboard.component.ts
@@ -13,15 +13,13 @@ export class CustomKeyboard implements OnInit, OnDestroy {
   prefixCls: string = 'am-number-keyboard';
   wrapCls: object;
   okText: string = '';
+  wrapperCls: object;
 
   private _locale: any = {};
   private _unsubscribe$ = new Subject<void>();
 
   @Output()
   onClick = new EventEmitter<any>();
-
-  @HostBinding('class.am-number-keyboard-wrapper')
-  private _wrapper = true;
 
   constructor(private _localeProvider: LocaleProviderService) {}
 
@@ -32,6 +30,9 @@ export class CustomKeyboard implements OnInit, OnDestroy {
   ngOnInit() {
     this.wrapCls = {
       [`${this.prefixCls}-item`]: true
+    };
+    this.wrapperCls = {
+      [`${this.prefixCls}-wrapper`]: true
     };
     this._localeProvider.localeChange.pipe(takeUntil(this._unsubscribe$)).subscribe(_ => {
       this._locale = this._localeProvider.getLocaleSubObj('InputItem');
