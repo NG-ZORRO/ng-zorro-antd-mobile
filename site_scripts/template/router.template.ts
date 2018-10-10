5a5e254006eaa2159609cd02baada7ec6e3923ad
Guoyuanqiang <guoyuanqiang2008@126.com>
2018-10-10 17:44:09 +0800

    chore: categorize all scripts
    chore: categorize all scripts

--- site_scripts/template/router.template.ts
+++
@@ -1,7 +0,0 @@
-export const ROUTER_LIST = {
-  'intro'     : {{intro}},
-  'components': {{components}}
-};
-export const DEMO_ROUTES = [
-{{routes}}
-];
--- site_scripts/utils/get-meta.js
+++
@@ -1,5 +0,0 @@
-const YFM = require('yaml-front-matter');
-
-module.exports = function getMeta(file) {
-  return YFM.loadFront(file);
-};
