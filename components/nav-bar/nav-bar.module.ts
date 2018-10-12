b1b580e0357f6a077ff50e2285e46e6e09384a06
nuonuoge <932273325@qq.com>
2018-10-12 18:04:03 +0800

    feat(module: nav-bar): add icon string support

--- components/nav-bar/nav-bar.module.ts
+++ components/nav-bar/nav-bar.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconModule } from '../icon/icon.module';
 import { NavBarComponent } from './nav-bar.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, IconModule],
   exports: [NavBarComponent],
   declarations: [NavBarComponent]
 })
