54f803688b145eefeb6a04a20bbb255de1afb9b9
BronzeCui <15120056@bjtu.edu.cn>
2018-10-23 10:49:55 +0800

    fix(packaging):fix the file path to generate theme.less (#72)

--- scripts/schematics/set-theme.js
+++ scripts/schematics/set-theme.js
@@ -1,10 +1,10 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const theme = fs.readFileSync(path.resolve(__dirname, `../../site/src/assets/color.less`), 'utf8');
+const theme = fs.readFileSync(path.resolve(__dirname, `../../components/style/themes/default.less`), 'utf8');
 fs.outputFileSync(
   path.resolve(__dirname, `../../schematics/utils/custom-theme.ts`),
-`export function createCustomTheme() {
+  `export function createCustomTheme() {
   return \`@import "../node_modules/ng-zorro-antd-mobile/src/ng-zorro-antd-mobile.less";
 ${theme.replace(/`/g, '\\`')}
 \`;
