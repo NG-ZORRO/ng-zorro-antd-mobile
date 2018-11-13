1e32a03d16fd76a0a557626abb1bc7dc85cb6345
Guoyuanqiang <guoyuanqiang2008@126.com>
2018-11-13 17:51:37 +0800

    fix(module:core): fix set providers of PopupService

--- components/core/core.module.ts
+++ components/core/core.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TouchFeedbackDirective } from './directive/touch-feedback.directive';
 import { TouchFeedBackComponent } from './directive/touch-feedback.component';
-
+import { PopupService } from './services/popup.service';
 export { PopupService } from './services/popup.service';
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [TouchFeedbackDirective, TouchFeedBackComponent],
-  declarations: [TouchFeedbackDirective, TouchFeedBackComponent]
+  declarations: [TouchFeedbackDirective, TouchFeedBackComponent],
+  providers: [PopupService]
 })
 export class CoreModule { }
