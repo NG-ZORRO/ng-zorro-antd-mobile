0702c959edafa7b662f8b40ea5ba3d941e949820
fisherspy <fisherspy@live.com>
2018-11-19 16:13:54 +0800

    fix(module: locale-provider): make locale-provider available cross modules;

--- components/locale-provider/locale-provider.module.ts
+++ components/locale-provider/locale-provider.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import zh_CN from './languages/zh_CN';
 import { LocaleProviderPipe } from './locale-provider.pipe';
 import { LOCAL_PROVIDER_TOKEN } from './locale-provider.token';
-import { LocaleProviderService } from './locale-provider.service';
+import { LOCALE_PROVIDER_SERVICE_PROVIDER } from './locale-provider.service';
 
 @NgModule({
   imports: [],
   declarations: [LocaleProviderPipe],
   exports: [LocaleProviderPipe],
-  providers: [{ provide: LOCAL_PROVIDER_TOKEN, useValue: zh_CN }, LocaleProviderService]
+  providers: [{ provide: LOCAL_PROVIDER_TOKEN, useValue: zh_CN }, LOCALE_PROVIDER_SERVICE_PROVIDER]
 })
 export class LocaleProviderModule {}
