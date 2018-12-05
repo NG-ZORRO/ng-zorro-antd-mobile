1ac6008847fb23dd6c6631e9e20ffe40be415b66
Zhenyun.Wu <huihui.whh@alibaba-inc.com>
2018-12-05 16:57:06 +0800

    fix(module: Searchbar): fix no provider for LocaleProviderService (#133)

--- components/search-bar/search-bar.module.ts
+++ components/search-bar/search-bar.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { SearchBar } from './search-bar.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { LocaleProviderModule } from '../locale-provider/locale-provider.module';
 
 @NgModule({
   exports: [SearchBar],
   declarations: [SearchBar],
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, LocaleProviderModule],
+  providers: [LocaleProviderModule]
 })
 export class SearchBarModule {}
