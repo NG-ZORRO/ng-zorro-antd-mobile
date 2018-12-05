bf75e17a04ffbc888b2b16310a7a4d05c7d343fc
3Fuyu <banxianyu@gmail.com>
2018-12-05 17:10:48 +0800

    fix(module: calendar): export calendar module&export local provider module for calendar (#134)

--- components/calendar/calendar.module.ts
+++ components/calendar/calendar.module.ts
@@ -9,10 +9,11 @@ import { ConfirmPanelComponent } from './confirm-panel/confirm-panel.component';
 import { SingleMonthComponent } from './single-month/single-month.component';
 import { ShortcutPanelComponent } from './shortcut-panel/shortcut-panel.component';
 import { DatePickerViewModule } from '../date-picker-view/date-picker-view.module';
+import { LocaleProviderModule } from '../locale-provider/locale-provider.module';
 import { IconModule } from '../icon/icon.module';
 
 @NgModule({
-  imports: [CommonModule, IconModule, DatePickerViewModule],
+  imports: [CommonModule, IconModule, DatePickerViewModule, LocaleProviderModule],
   declarations: [
     CalendarComponent,
     HeaderComponent,
@@ -32,6 +33,7 @@ import { IconModule } from '../icon/icon.module';
     ConfirmPanelComponent,
     TimePickerComponent,
     ShortcutPanelComponent
-  ]
+  ],
+  providers: [LocaleProviderModule]
 })
 export class CalendarModule {}
