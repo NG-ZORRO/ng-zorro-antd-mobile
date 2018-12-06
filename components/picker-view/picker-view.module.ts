6779f2fb1115446160dfa128b090dd52a8db9f07
Guoyuanqiang <guoyuanqiang2008@126.com>
2018-12-06 10:12:58 +0800

    feat(module:PickerView): import PickerModule and  LocaleProviderModule

--- components/picker-view/picker-view.module.ts
+++ components/picker-view/picker-view.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PickerViewComponent } from './picker-view.component';
-
+import { PickerModule } from '../picker/picker.module';
+import { LocaleProviderModule } from '../locale-provider/locale-provider.module';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PickerModule,
+    LocaleProviderModule
   ],
   exports: [PickerViewComponent],
   declarations: [PickerViewComponent]
