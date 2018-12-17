b4af7c7f271213a69e3bc976a60760198c79523b
Liyulun <18217551067@163.com>
2018-12-17 15:46:17 +0800

    feat(module: picker): picker ngModel support (#165)
    * feat(module: picker): picker ngModel support
    
    * feat(module: picker): add  docs
    
    * feat(module: picker): add test
    
    * feat(module: picker): modifed api
    
    * feat(module: picker): remove outdated api [value] and (onOk)
    
    * feat(module: locale-provider): modified picker directive api

--- components/picker/picker.directive.ts
+++ components/picker/picker.directive.ts
@@ -14,22 +14,30 @@ import {
   ComponentFactoryResolver,
   Renderer2,
   ComponentFactory,
-  SimpleChanges
+  SimpleChanges,
+  forwardRef
 } from '@angular/core';
 import { PickerComponent } from './picker.component';
 import { PickerOptions } from './picker-options.provider';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
-  selector: '[Picker], [nzm-picker]'
+  selector: '[Picker], [nzm-picker]',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PickerDirective),
+      multi: true
+    }
+  ]
 })
-export class PickerDirective implements OnDestroy, OnChanges, OnInit {
+export class PickerDirective implements OnDestroy, OnChanges, OnInit, ControlValueAccessor {
   picker: ComponentRef<PickerComponent>;
+  value: Array<any>;
   private _eventListeners: Array<() => void> = [];
 
   @Input()
   data: Array<any>;
-  @Input()
-  value: Array<any>;
   @Input()
   cols: Number;
   @Input()
@@ -55,9 +63,10 @@ export class PickerDirective implements OnDestroy, OnChanges, OnInit {
   @Output()
   onPickerChange: EventEmitter<any> = new EventEmitter();
   @Output()
-  onOk: EventEmitter<any> = new EventEmitter();
-  @Output()
   onDismiss: EventEmitter<any> = new EventEmitter();
+
+  onChange: (value: any[]) => void = () => null;
+  onTouched: () => void = () => null;
 
   @HostListener('click')
   togglePicker(): void {
@@ -117,6 +126,9 @@ export class PickerDirective implements OnDestroy, OnChanges, OnInit {
       Object.assign(options, this._defaultOptions, {
         hidePicker: (event): void => {
           this.hidePicker();
+        },
+        updateNgModel: (value: any[]): void => {
+          this.onChange(value);
         }
       });
 
@@ -132,7 +144,6 @@ export class PickerDirective implements OnDestroy, OnChanges, OnInit {
         'cascade',
         'appendToBody',
         'indicatorStyle',
-        'onOk',
         'onPickerChange'
       ];
       optionalParams.forEach(param => {
@@ -167,5 +178,21 @@ export class PickerDirective implements OnDestroy, OnChanges, OnInit {
       this._eventListeners.forEach(fn => fn());
       this._eventListeners = [];
     }
+  }
+
+  writeValue(value: any[]): void {
+    this.value = Array.isArray(value) ? value : [];
+  }
+
+  registerOnChange(fn: (value: any[]) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
   }
 }
