292786271fa928ce6dcf433498c7e74b11d7fc5b
fisherspy <fisherspy@live.com>
2019-01-02 19:42:18 +0800

    fix(module: website): update website support version

--- scripts/site/_site/src/app/app.component.ts
+++ scripts/site/_site/src/app/app.component.ts
@@ -21,8 +21,8 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   language = 'zh';
-  versionList = ['0.9.x'];
-  currentVersion = '0.9.x';
+  versionList = ['0.11.x'];
+  currentVersion = '0.11.x';
   public isHomeURL = true;
   public isKitchenURL = false;
   public demoTitle = '';
