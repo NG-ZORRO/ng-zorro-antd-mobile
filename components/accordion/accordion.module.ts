1423cbaf2c5fc07c6d2d5a55f9895f706d89be7b
Guoyuanqiang <guoyuanqiang2008@126.com>
2019-02-27 11:34:57 +0800

    fix(module: accordion): fix accordion support template and add support innerHtml (#307)

--- components/accordion/accordion.module.ts
+++ components/accordion/accordion.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AccordionComponent } from './accordion.component';
 import { AccordionGroupComponent } from './accordion-group/accordion-group.component';
 import { WhiteSpaceModule } from '../white-space/white-space.module';
+import { NgZorroAntdMobilePipesModule } from '../pipes/ng-zorro-antd-mobile.pipes.module';
 
 @NgModule({
-  imports: [CommonModule, WhiteSpaceModule],
+  imports: [CommonModule, WhiteSpaceModule, NgZorroAntdMobilePipesModule],
   declarations: [AccordionComponent, AccordionGroupComponent],
   exports: [AccordionComponent, AccordionGroupComponent]
 })
--- components/accordion/demo/basic.ts
+++ components/accordion/demo/basic.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 
 @Component({
   selector: 'demo-accordion-basic',
@@ -19,18 +19,27 @@ import { Component } from '@angular/core';
         </List>
       </AccordionPanel>
     </Accordion>
+    <ng-template #title1>
+      <div>Title 1</div>
+    </ng-template>
   `
 })
-export class DemoAccordionBasicComponent {
-  accordions: Array<any> = [
-    { title: 'Title 1', child: ['content 1', 'content 1', 'content 1'] },
-    { title: 'Title 2', child: ['content 2', 'content 2', 'content 2'], inactive: false },
-    { title: 'Title 3', child: ['content 3', 'content 3', 'content 3'], inactive: true }
-  ];
+export class DemoAccordionBasicComponent implements OnInit {
+  @ViewChild('title1') title1: ViewChild;
+  accordions: Array<any> = [];
 
   activeKey = [0, 1];
 
   onChange(event) {
     console.log(event);
   }
+
+  ngOnInit() {
+    this.accordions = [
+      { title: this.title1, child: ['content 1', 'content 1', 'content 1'] },
+      { title: `<img src="/assets/icons/icon-72x72.png" style="width:36px"/>`, child: ['content 2', 'content 2', 'content 2'], inactive: false },
+      { title: 'Title 3', child: ['content 3', 'content 3', 'content 3'], inactive: true }
+    ];
+  }
+
 }
