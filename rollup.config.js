826720a13668d2d3a51d0751d4181397e6b5036b
DreamPWJ <pwjay.ok@163.com>
2019-04-26 18:58:50 +0800

    chore: code specification optimization (#396)

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
