35772d34dea28be28f52a8c9e0d14d6d02055736
安泊尔 <shuangyan_m@hotmail.com>
2019-05-30 11:15:30 +0800

    fix(module: radio-item-group): fix checked error.

--- components/ng-zorro-antd-mobile.module.ts
+++ components/ng-zorro-antd-mobile.module.ts
@@ -98,25 +98,6 @@ export * from './date-picker-view/public-api';
 export * from './segmented-control/public-api';
 export * from './activity-indicator/public-api';
 
-// services
-// action-sheet
-export * from './action-sheet/action-sheet.service';
-export * from './action-sheet/action-sheet-subject.service';
-export * from './action-sheet/action-sheet-options.provider';
-// locale-provider
-export * from './locale-provider/locale-provider.service';
-export * from './locale-provider/locale-provider.token';
-// modal
-export * from './modal/modal.service';
-export * from './modal/modal-options.provider';
-// picker
-export * from './picker/picker.service';
-export * from './picker/picker-options.provider';
-// toast
-export * from './toast/toast.service';
-export * from './toast/toast-options.provider';
-
-
 @NgModule({
   providers: [],
   exports: [
