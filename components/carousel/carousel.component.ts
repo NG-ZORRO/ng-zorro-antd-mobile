0c7c0066a3f5097c37247170516b4ec4a82f9ad2
Guoyuanqiang <guoyuanqiang2008@126.com>
2019-09-14 17:10:29 +0800

    fix(module: carousel): fix carousel style int other content error (#582)

--- components/carousel/carousel.component.ts
+++ components/carousel/carousel.component.ts
@@ -416,7 +416,7 @@ export class CarouselComponent implements AfterViewInit, OnDestroy {
       this.cellSpacing;
     this.style = {
       height: this._currentSlideHeight + 'px',
-      width: this.vertical ? this._currentSlideWidth + 'px' : this.items.length * this._rationWidth + 'px',
+      width: '100%',
       transform: this.vertical
         ? `translate3d(0px, ${positionOffset}px, 0px)`
         : `translate3d(${positionOffset}px, 0px, 0px)`,
