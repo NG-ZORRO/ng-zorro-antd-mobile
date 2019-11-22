b7b878a9401cf66f9a22e275b113a5de030f5e64
nuonuoge <932273325@qq.com>
2019-11-22 13:18:41 +0800

    fix(module: steps): fix step import async

--- components/steps/steps.component.ts
+++ components/steps/steps.component.ts
@@ -138,8 +138,11 @@ export class StepsComponent implements OnInit, AfterContentInit {
   }
 
   ngAfterContentInit() {
-    setTimeout(() => {
-      this.setStepStyle();
-    }, 0);
+    this.stepItems.changes.subscribe(change => {
+      setTimeout(() => {
+        this.setStepStyle();
+      }, 0);
+    });
   }
+
 }
