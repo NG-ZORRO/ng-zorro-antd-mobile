bcbd861e5cc96190f7c8260848ac309c5d4348d5
tirelyl <466214245@qq.com>
2020-04-03 20:22:17 +0800

    fix: site github-btn style

--- scripts/site/utils/angular-nonbindable.js
+++ scripts/site/utils/angular-nonbindable.js
@@ -1,6 +1,6 @@
 module.exports = function angularNonBindAble(content, name) {
   if (name && name.includes('introduce')) {
-    return content;
+    return content.replace(/<style>([\s\S]*)<\/style>/g, '');
   } else {
     return content.replace(/{/g, '&#123;').replace(/}/g, '&#125;');
   }
