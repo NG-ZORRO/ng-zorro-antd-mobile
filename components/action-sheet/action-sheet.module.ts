c5da92037e5c448eca3c27d09aae3d425eadb279
nuonuoge <932273325@qq.com>
2020-04-17 14:50:15 +0800

    fix(module: action-sheet): import TouchFeedbackDirective (#669)

--- components/action-sheet/action-sheet.module.ts
+++ components/action-sheet/action-sheet.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OverlayModule } from '@angular/cdk/overlay';
 import { ListModule } from '../list/list.module';
 import { NgZorroAntdMobilePipesModule } from '../pipes/ng-zorro-antd-mobile.pipes.module';
 import { ActionSheetComponent } from './action-sheet.component';
 import { WingBlankModule } from '../wing-blank/wing-blank.module';
 import { WhiteSpaceModule } from '../white-space/white-space.module';
 import { LocaleProviderModule } from '../locale-provider/locale-provider.module';
+import { TouchFeedbackModule } from '../core/directive/touch-feedback.module';
 import { PopupService } from '../core/services/popup.service';
 import { ActionSheetService } from './action-sheet.service';
-import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { OverlayModule } from '@angular/cdk/overlay';
     ListModule,
     WhiteSpaceModule,
     WingBlankModule,
-    LocaleProviderModule
+    LocaleProviderModule,
+    TouchFeedbackModule
   ],
   declarations: [ActionSheetComponent],
   exports: [ActionSheetComponent],
