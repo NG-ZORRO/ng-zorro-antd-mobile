4117430981a426d6175f4c3e4322ea60d665d72c
fisherspy <fisherspy@live.com>
2020-06-15 15:48:39 +0800

    release(2.0.2): release 2.0.2

--- scripts/site/_site/src/app/app.component.ts
+++ scripts/site/_site/src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   docsearch = null;
   kitchenUrl = window.location.origin + '/#/kitchen-sink?lang=zh-CN';
   language = 'zh';
-  versionList = ['0.12.x', '1.0.1', '1.0.2', '1.0.3', '1.0.4', '1.0.5', '2.0.1'];
+  versionList = ['0.12.x', '1.0.1', '1.0.2', '1.0.3', '1.0.4', '1.0.5', '1.0.6', '2.0.1', '2.0.2'];
   versionMap = {
     '0.12.x': '0.12.5',
     '1.0.1': '1.0.11',
@@ -27,9 +27,11 @@ export class AppComponent implements OnInit, AfterViewInit {
     '1.0.3': '1.0.3',
     '1.0.4': '1.0.4',
     '1.0.5': '1.0.51',
-    '2.0.1': '2.0.100'
+    '1.0.6': '2001.0.6',
+    '2.0.1': '2.0.100',
+    '2.0.2': '2.0.2'
   };
-  currentVersion = '2.0.1';
+  currentVersion = '2.0.2';
   isHomeURL = true;
   isKitchenURL = false;
   demoTitle = '';
