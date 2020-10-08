ae2652f6b8353278c8744ea87ea4dde577c74498
Eve <948832626@qq.com>
2020-10-08 13:48:02 +0800

    chore: bump zorro to v10

--- scripts/site/_site/src/app/share/nz-codebox/nz-codebox.module.ts
+++ scripts/site/_site/src/app/share/nz-codebox/nz-codebox.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { NzCodeBoxComponent } from './nz-codebox.component';
 import { CommonModule } from '@angular/common';
 import { NzHighlightModule } from '../nz-highlight/nz-highlight.module';
-import { NzIconModule, NzModalModule, NzToolTipModule } from 'ng-zorro-antd';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
 const ngZorroModules = [
   NzIconModule,
--- scripts/site/_site/src/app/share/nz-nav-bottom/nz-nav-bottom.module.ts
+++ scripts/site/_site/src/app/share/nz-nav-bottom/nz-nav-bottom.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NzIconModule } from 'ng-zorro-antd';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzNavBottomComponent } from './nz-nav-bottom.component';
 
 @NgModule({
