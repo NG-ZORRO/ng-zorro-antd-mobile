5c4a83389a667b5f2c9878cc1491f0b43fbc5452
nuonuoge <932273325@qq.com>
2020-11-19 18:55:00 +0800

    fix(module: steps): add current support

--- components/steps/step/step.component.ts
+++ components/steps/step/step.component.ts
@@ -25,14 +25,19 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
 export class StepComponent implements OnInit {
   prefixCls = 'am-steps';
   stepItemCls = {};
-  iconCls: object;
+  iconCls: object = {
+    [`${this.prefixCls}-icon`]: true
+  };
   tailContent: string;
   stepNumber: number;
   isIconString: boolean = true;
   iconSize: string;
+  outStatus = 'process';
 
   private _status: StepStatusEnum;
   private _icon: string | TemplateRef<any>;
+  private isCustomStatus = false;
+  private isCustomIcon = false;
 
   @Input()
   get status(): StepStatusEnum {
@@ -41,6 +46,8 @@ export class StepComponent implements OnInit {
   set status(value: StepStatusEnum) {
     if (value) {
       this._status = value;
+      this.isCustomStatus = true;
+      this.setIcon();
       this.setClass();
     }
   }
@@ -55,30 +62,58 @@ export class StepComponent implements OnInit {
   set icon(value: string | TemplateRef<any>) {
     if (value) {
       this._icon = value;
+      this.isCustomIcon = true;
       this.setClass();
     }
   }
 
+  set currentIndex(current: number) {
+    this._currentIndex = current;
+    if (!this.isCustomStatus) {
+      this._status = current > this.stepNumber ? StepStatusEnum.FINISH : current === this.stepNumber ?
+        this.outStatus || ('' as any) : StepStatusEnum.WAIT;
+      this.setIcon();
+      this.setClass();
+    }
+  }
+
+  private _currentIndex = 0;
+
   @HostBinding('class.am-steps-item')
   clsStepItem: boolean = true;
 
-  constructor(private _el: ElementRef) {}
+  constructor(private _el: ElementRef) { }
 
   isTemplateRef(value) {
     return value instanceof TemplateRef;
   }
 
   setClass() {
-    this.iconCls = {
-      [`${this.prefixCls}-icon`]: true
-    };
-    this.stepItemCls = Object.assign(this.stepItemCls, {
+    this.stepItemCls = {
       [`${this.prefixCls}-item-${this.status}`]: true,
-      [`${this.prefixCls}-item-custom`]: this.icon
-    });
+      [`${this.prefixCls}-item-custom`]: this.isCustomIcon || (this.icon && this._currentIndex !== this.stepNumber)
+    };
   }
 
-  ngOnInit() {}
+  setIcon() {
+    if (!this.isCustomIcon) {
+      switch (this._status) {
+        case StepStatusEnum.FINISH:
+          this._icon = 'check-circle-o';
+          break;
+        case StepStatusEnum.ERROR:
+          this._icon = 'cross-circle-o';
+          break;
+        case StepStatusEnum.WAIT:
+          this._icon = 'ellipsis';
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  ngOnInit() { }
 }
 
 export enum StepStatusEnum {
--- components/steps/steps.component.ts
+++ components/steps/steps.component.ts
@@ -23,7 +23,6 @@ export class StepsComponent implements OnInit, AfterContentInit {
   private _size: string;
   private _status: StepStatusEnum = StepStatusEnum.PROCESS;
   private _direction: StepDirectionEnum = StepDirectionEnum.VERTICAL;
-  private _stepsCls: object;
 
   @ContentChildren(StepComponent)
   stepItems: QueryList<StepComponent>;
@@ -32,6 +31,9 @@ export class StepsComponent implements OnInit, AfterContentInit {
   set current(value) {
     if (value >= 0) {
       this._current = value;
+      if (this.stepItems) {
+        this.setStepStyle();
+      }
     }
   }
   @Input()
@@ -63,7 +65,7 @@ export class StepsComponent implements OnInit, AfterContentInit {
   @HostBinding('class.am-steps-horizontal')
   clsStepsHztl: boolean;
 
-  constructor(private _elf: ElementRef, private _render: Renderer2) {}
+  constructor(private _elf: ElementRef, private _render: Renderer2) { }
 
   setStepStyle() {
     const itemCount = this.stepItems.length;
@@ -71,49 +73,14 @@ export class StepsComponent implements OnInit, AfterContentInit {
     for (let index = 0; index < itemCount; index++) {
       const step = itemArr[index];
       step.stepNumber = index + 1;
+      step.outStatus = this._status;
+      step.currentIndex = this._current + 1;
+      step.iconSize = this._size === 'small' ? (this._status === StepStatusEnum.WAIT ? 'xxs' : 'xs') : 'md';
       if (index < itemCount - 1 && itemArr[index + 1].status === StepStatusEnum.ERROR) {
         step.stepItemCls = step.stepItemCls
           ? Object.assign(step.stepItemCls, { 'error-tail': true })
           : { 'error-tail': true };
       }
-      let icon = step.icon;
-      if (!step.status) {
-        if (index === this._current) {
-          step.status = this._status;
-        } else if (index < this._current) {
-          step.status = StepStatusEnum.FINISH;
-        } else {
-          step.status = StepStatusEnum.WAIT;
-        }
-      } else if (step.status && !icon) {
-        switch (step.status) {
-          case StepStatusEnum.FINISH:
-            icon = 'check-circle-o';
-            break;
-          case StepStatusEnum.ERROR:
-            icon = 'cross-circle-o';
-            break;
-        }
-      }
-      if (!icon && step.status !== StepStatusEnum.PROCESS) {
-        if (index < this._current) {
-          icon = 'check-circle-o';
-        } else if (index > this._current) {
-          icon = 'ellipsis';
-          step.stepItemCls = step.stepItemCls
-            ? Object.assign(step.stepItemCls, { 'ellipsis-item': true })
-            : { 'ellipsis-item': true };
-        }
-        if (
-          (this._status === StepStatusEnum.ERROR && index === this._current) ||
-          step.status === StepStatusEnum.ERROR
-        ) {
-          icon = 'cross-circle-o';
-        }
-      }
-      step.icon = icon;
-      step.iconSize = this._size === 'small' ? (this._status === StepStatusEnum.WAIT ? 'xxs' : 'xs') : 'md';
-      step.setClass();
     }
   }
 
