ae7522137ffc3572b0bd6cefbf26cdeec41bd966
°))))彡 <fisherspy@live.com>
2021-01-04 11:26:17 +0800

    release(4.0.0): release 4.0.0 (#775)

--- scripts/site/_site/src/app/share/nz-codebox/nz-codebox.component.ts
+++ scripts/site/_site/src/app/share/nz-codebox/nz-codebox.component.ts
@@ -340,19 +340,19 @@ export class AppModule { }
     description: 'Created with <3 by the StackBlitz SDK!',
     template: 'angular-cli',
     dependencies: {
-      '@angular/animations': '^10.0.14',
-      '@angular/cdk': '^10.1.3',
-      '@angular/common': '^10.0.14',
-      '@angular/compiler': '^10.0.14',
-      '@angular/core': '^10.0.14',
-      '@angular/forms': '^10.0.14',
-      '@angular/platform-browser': '^10.0.14',
-      '@angular/platform-browser-dynamic': '^10.0.14',
-      '@angular/router': '^10.0.14',
-      '@ant-design/icons-angular': '^10.0.1',
+      '@angular/animations': '^11.0.5',
+      '@angular/cdk': '^11.0.3',
+      '@angular/common': '^11.0.5',
+      '@angular/compiler': '^11.0.5',
+      '@angular/core': '^11.0.5',
+      '@angular/forms': '^11.0.5',
+      '@angular/platform-browser': '^11.0.5',
+      '@angular/platform-browser-dynamic': '^11.0.5',
+      '@angular/router': '^11.0.5',
+      '@ant-design/icons-angular': '^11.0.1',
       'rxjs': '~6.6.2',
       'zone.js': '~0.10.2',
-      'ng-zorro-antd-mobile': '3.0.3'
+      'ng-zorro-antd-mobile': '4.0.0'
     },
     tags: ['stackblitz', 'sdk']
   });
