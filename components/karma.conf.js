fcfd54fe690e48f5410843c9b474b5dd1d9bb843
nuonuoge <932273325@qq.com>
2021-07-12 16:43:50 +0800

    feat(module: all): support Angular 12

--- components/karma.conf.js
+++ components/karma.conf.js
@@ -9,15 +9,19 @@ module.exports = function(config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+    coverageReporter: {
+      subdir: '.',
+      dir: require('path').join(__dirname, '../coverage'),
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' },
+      ],
     },
     angularCli: {
       environment: 'dev'
